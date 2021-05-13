422fca1889dc881bdc64f93704a5922150c0aded
chrism <chrism@kingorange.com>
2021-05-13 16:22:16 -0700

    exam-end

--- Final.py
+++ Final.py
@@ -29,3 +29,32 @@ def calculate_percent_above_average():
     percent_above_average = above_average / num_of_lines
     print(percent_above_average)
 """
+
+def main():
+    with open('Final.txt') as infile:
+        lines = infile.readlines()
+        infile.close()
+    calculate_percent_above_average(lines)
+
+
+def calculate_percent_above_average(input):
+    for i in range(len(input)):
+        input[i] = int(input[i])
+
+    num_of_lines = len(input)
+    Sum = sum(input)
+    average = Sum / num_of_lines
+    
+    above_average = 0
+    for i in range(len(input)):
+        if input[i] > average:
+            above_average += 1
+
+    percent_above_average = (above_average / num_of_lines) * 100
+    percent_above_average = round(percent_above_average, 2)
+
+    print(f"{num_of_lines} Provided")
+    print(f"The Average Grade was a {average}%")
+    print(f"{percent_above_average}% grades were higher than the average")
+
+main()
